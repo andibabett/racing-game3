5a2ba279e485dc996a87e49915e7d7c64151d5b0
andibabett <andi_babett@yahoo.com>
2019-09-30 19:40:32 +0300

    nou

--- src/main/java/org/fasttrackit/Game.java
+++ src/main/java/org/fasttrackit/Game.java
@@ -54,6 +54,8 @@ public class Game {
 
     }
 
+
+
     private double getAccelerationSpeedFromUser() {
         System.out.println("Please enter acceleration speed:");
         Scanner scanner = new Scanner(System.in);
